2e4a235bf09a08ca35e105e5c26817aefa73b1f4
Devesh <deepubhushan123@gmail.com>
2021-04-22 17:13:20 +0530

    added sign i oage and sign in with google and facebook

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Switch } from 'react-router';
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
+import 'rsuite/dist/styles/rsuite-default.css';
+
+import './styles/main.scss';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 
