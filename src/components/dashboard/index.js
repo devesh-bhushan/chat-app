d712561854308892ee2f84f4002c17d3f066575b
Devesh <deepubhushan123@gmail.com>
2021-04-24 15:25:06 +0530

    updated nickname inside the database

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { Button, Drawer, Divider } from 'rsuite';
+import { Button, Drawer, Divider, Alert } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
 import EditableInput from '../EditableInput';
+import { database } from '../../misc/firebase';
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
   const onSave = async newData => {
-    console.log(newData);
+    const userNicknameRef = database
+      .ref(`/profiles/${profile.uid}`)
+      .child('name');
+
+    try {
+      await userNicknameRef.set(newData);
+      Alert.success('Nickname has been updated', 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
   };
 
   return (
