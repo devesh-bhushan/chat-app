5d05936fcba6011212a224a56f506f5fafe7769f
Devesh <deepubhushan123@gmail.com>
2021-04-25 02:24:57 +0530

    Started to work with user avatar

---
+++ src/components/dashboard/AvatarUploadBtn.js
@@ -0,0 +1,74 @@
+/* eslint-disable no-unused-vars */
+import React, { useState, useRef } from 'react';
+import { Alert, Button, Modal } from 'rsuite';
+import AvatarEditor from 'react-avatar-editor';
+import { useModalState } from '../../misc/custom-hooks';
+
+const fileInputTypes = '.png, .jpeg, .jpg ';
+const acceptedFileTypes = ['image/png', 'image/jpeg', 'image/pjpeg'];
+const isValidFile = file => acceptedFileTypes.includes(file.type);
+
+const AvatarUploadBtn = () => {
+  const [img, setImg] = useState(null);
+  const { isOpen, open, close } = useModalState();
+
+  const onFileInputChange = ev => {
+    const currFiles = ev.target.files;
+    if (currFiles.length === 1) {
+      const file = currFiles[0];
+      if (isValidFile(file)) {
+        setImg(file);
+
+        open();
+      } else {
+        Alert.warning(`Wrong file type ${file.type}`, 4000);
+      }
+    }
+  };
+
+  return (
+    <div className=" mt-3 text-center">
+      <div>
+        <label
+          htmlFor="avatar-upload"
+          className="d-block cursor-pointer padded"
+        >
+          Select New Avatar
+          <input
+            id="avatar-upload"
+            type="file"
+            className="d-none"
+            accept={fileInputTypes}
+            onChange={onFileInputChange}
+          />
+        </label>
+        <Modal show={isOpen} onHide={close}>
+          <Modal.Header>
+            <Modal.Title>Adjust and Uplaod New Avatar</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="d-flex justify-content-center align-item-center h-100">
+              {img && (
+                <AvatarEditor
+                  image={img}
+                  width={200}
+                  height={200}
+                  border={10}
+                  borderRadius={100}
+                  rotate={0}
+                />
+              )}
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button block appearance="ghost">
+              Upload New Avatar
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    </div>
+  );
+};
+
+export default AvatarUploadBtn;
